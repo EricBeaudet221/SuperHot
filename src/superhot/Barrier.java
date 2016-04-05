1d868c5397adbf1373c3054814e58730e115ba94
cbalme <colin.balme@brentwood.ca>
2016-04-05 10:47:14 -0700

    got barriers working

---
+++ src/superhot/Barrier.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superhot;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author Oliver
+ */
+public class Barrier {
+
+//<editor-fold defaultstate="collapsed" desc="Drawing">
+    public void draw(Graphics graphics){
+        graphics.setColor(color);
+        graphics.fill3DRect(x, y, width, height, true);
+    }
+    
+//</editor-fold>
+    
+//<editor-fold defaultstate="collapsed" desc="Constructors">
+    public Barrier(int x, int y, int width, int height, Color color, boolean breakable){
+        this.x= x;
+        this.y= y;
+        this.height= height;
+        this.width= width;
+        this.color= color;
+        this.breakable = breakable;
+    }
+//</editor-fold>
+      
+//<editor-fold defaultstate="collapsed" desc="Properties">
+    private int x, y, width, height;
+    private Color color;
+    private boolean breakable = false;
+    
+    
+    
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+    
+    /**
+     * @return the x
+     */
+    public Point getLocation() {
+        return new Point(x, y);
+    }
+    
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+    
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+    
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
+
+//</editor-fold>
--- src/superhot/SHworld.java
+++ src/superhot/SHworld.java
@@ -6,15 +6,38 @@
 package superhot;
 
 import environment.Environment;
+import grid.Grid;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  *
  * @author ericbeaudet
  */
-public class SHworld extends Environment{
+public class SHworld extends Environment {
+
+    Grid grid;
+    private ArrayList<Barrier> barriers;
+
+    public SHworld() {
+        barriers = new ArrayList<>();
+             createBarrierRange(0, 13, 38, 1000, new Color(154, 205, 50), true);
+           createBarrierRange(-1, -1, -1, 22, new Color(153, 0, 76, 255), true);
+            createBarrierRange(-1, -1, 39, -1, new Color(0, 0, 128, 255), true);
+            createBarrierRange(-1, 22, 39, 22, new Color(0, 0, 128, 255), true);
+        createBarrierRange(39, 0, 1000, 1000, new Color(153, 76, 0, 255), true);
+    }
+
+    public void createBarrierRange(int startX, int startY, int endX, int endY, Color color, boolean breakable) {
+        for (int x = startX; x <= endX; x++) {
+            for (int y = startY; y <= endY; y++) {
+                barriers.add(new Barrier(x, y, 10, 10, color, breakable));
+            }
+        }
+    }
 
     @Override
     public void initializeEnvironment() {
@@ -38,6 +61,13 @@ public class SHworld extends Environment{
 
     @Override
     public void paintEnvironment(Graphics graphics) {
+        if (barriers != null) {
+            for (int i = 0; i < barriers.size(); i++) {
+                barriers.get(i).draw(graphics);
+            }
+        }
     }
-    
+
+
+
 }
