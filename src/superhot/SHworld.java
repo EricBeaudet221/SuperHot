a22c1e2557fec29ade3e3bed208bf9843e3708e7
EricBeaudet221 <eric.beaudet@brentwood.ca>
2016-04-08 08:53:55 -0700

    added one background 4 level 1

--- src/superhot/SHworld.java
+++ src/superhot/SHworld.java
@@ -6,6 +6,7 @@
 package superhot;
 
 import environment.Environment;
+import images.ResourceTools;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -15,6 +16,11 @@ import java.awt.event.MouseEvent;
  * @author ericbeaudet
  */
 public class SHworld extends Environment{
+    
+    
+    public SHworld() {
+        this.setBackground(ResourceTools.loadImageFromResource("SuperHot/level_1.png"));
+    }
 
     @Override
     public void initializeEnvironment() {
