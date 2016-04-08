503f79a0f53f06fba099d2103eabd0345ca45f14
cbalme <colin.balme@brentwood.ca>
2016-04-08 09:03:01 -0700

    Level Manager began

--- src/superhot/GameState.java
+++ src/superhot/GameState.java
@@ -10,5 +10,5 @@ package superhot;
  * @author Oliver
  */
 public enum GameState {
-    
+    LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, LEVEL_6, LEVEL_7, LEVEL_8, LEVEL_9, LEVEL_10, LEVEL_11
 }
--- src/superhot/LevelManager.java
+++ src/superhot/LevelManager.java
@@ -5,10 +5,37 @@
  */
 package superhot;
 
+import images.ImageManager;
+import java.util.ArrayList;
+
 /**
  *
  * @author Oliver
  */
-public class LevelManager {
+public class LevelManager extends ImageManager {
 
+    public LevelManager() {
+        LEVEL_1_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_2_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_3_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_4_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_5_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_6_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_7_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_8_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_9_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_10_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_11_IMAGE_NAMES = new ArrayList<>();
+    }
+    public static ArrayList<String> LEVEL_1_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_2_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_3_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_4_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_5_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_6_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_7_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_8_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_9_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_10_IMAGE_NAMES;
+    public static ArrayList<String> LEVEL_11_IMAGE_NAMES;
 }
