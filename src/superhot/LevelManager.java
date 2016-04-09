02d2c88a11cdcb7ba30b5c60d30d6da408909ebb
cbalme <colin.balme@brentwood.ca>
2016-04-09 11:40:09 -0700

    finished images for level manager, need to make grid

--- src/superhot/LevelManager.java
+++ src/superhot/LevelManager.java
@@ -6,6 +6,8 @@
 package superhot;
 
 import images.ImageManager;
+import images.ResourceTools;
+import java.awt.Color;
 import java.util.ArrayList;
 
 /**
@@ -16,17 +18,54 @@ public class LevelManager extends ImageManager {
 
     public LevelManager() {
         LEVEL_1_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_1_IMAGE_NAMES.add(LEVEL_1);
+        createBarrierRange(0, 13, 38, 1000, new Color(154, 205, 50), true);
         LEVEL_2_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_2_IMAGE_NAMES.add(LEVEL_2);
         LEVEL_3_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_3_IMAGE_NAMES.add(LEVEL_3);
         LEVEL_4_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_4_IMAGE_NAMES.add(LEVEL_4);
         LEVEL_5_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_5_IMAGE_NAMES.add(LEVEL_5);
         LEVEL_6_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_6_IMAGE_NAMES.add(LEVEL_6);
         LEVEL_7_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_7_IMAGE_NAMES.add(LEVEL_7);
         LEVEL_8_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_8_IMAGE_NAMES.add(LEVEL_8);
         LEVEL_9_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_9_IMAGE_NAMES.add(LEVEL_9);
         LEVEL_10_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_10_IMAGE_NAMES.add(LEVEL_10);
         LEVEL_11_IMAGE_NAMES = new ArrayList<>();
+        LEVEL_11_IMAGE_NAMES.add(LEVEL_11);
+
+        this.addImage(LEVEL_1, ResourceTools.loadImageFromResource("superhot/level_1.png"));
+        this.addImage(LEVEL_2, ResourceTools.loadImageFromResource("superhot/tutorial_level_1.png"));
+        this.addImage(LEVEL_3, ResourceTools.loadImageFromResource("superhot/tutorial_level_1.png"));
+        this.addImage(LEVEL_4, ResourceTools.loadImageFromResource("superhot/tutorial_level_1.png"));
+        this.addImage(LEVEL_5, ResourceTools.loadImageFromResource("superhot/tutorial_level_1.png"));
+        this.addImage(LEVEL_6, ResourceTools.loadImageFromResource("superhot/tutorial_level_1.png"));
+        this.addImage(LEVEL_7, ResourceTools.loadImageFromResource("superhot/level_2"));
+        this.addImage(LEVEL_8, ResourceTools.loadImageFromResource("superhot/level_3"));
+        this.addImage(LEVEL_9, ResourceTools.loadImageFromResource("superhot/level_4"));
+        this.addImage(LEVEL_10, ResourceTools.loadImageFromResource("superhot/boss_level.png"));
+        this.addImage(LEVEL_11, ResourceTools.loadImageFromResource("superhot/endless_level.png"));
     }
+
+    public static final String LEVEL_1 = "LEVEL_1";
+    public static final String LEVEL_2 = "LEVEL_2";
+    public static final String LEVEL_3 = "LEVEL_3";
+    public static final String LEVEL_4 = "LEVEL_4";
+    public static final String LEVEL_5 = "LEVEL_5";
+    public static final String LEVEL_6 = "LEVEL_6";
+    public static final String LEVEL_7 = "LEVEL_7";
+    public static final String LEVEL_8 = "LEVEL_8";
+    public static final String LEVEL_9 = "LEVEL_9";
+    public static final String LEVEL_10 = "LEVEL_10";
+    public static final String LEVEL_11 = "LEVEL_11";
+
     public static ArrayList<String> LEVEL_1_IMAGE_NAMES;
     public static ArrayList<String> LEVEL_2_IMAGE_NAMES;
     public static ArrayList<String> LEVEL_3_IMAGE_NAMES;
@@ -38,4 +77,14 @@ public class LevelManager extends ImageManager {
     public static ArrayList<String> LEVEL_9_IMAGE_NAMES;
     public static ArrayList<String> LEVEL_10_IMAGE_NAMES;
     public static ArrayList<String> LEVEL_11_IMAGE_NAMES;
+
+    private ArrayList<Barrier> barriers;
+
+    public void createBarrierRange(int startX, int startY, int endX, int endY, Color color, boolean breakable) {
+        for (int x = startX; x <= endX; x++) {
+            for (int y = startY; y <= endY; y++) {
+                barriers.add(new Barrier(x, y, 10, 10, color, breakable));
+            }
+        }
+    }
 }
--- src/superhot/SHworld.java
+++ src/superhot/SHworld.java
@@ -23,12 +23,12 @@ public class SHworld extends Environment {
     private ArrayList<Barrier> barriers;
 
     public SHworld() {
-        barriers = new ArrayList<>();
-             createBarrierRange(0, 13, 38, 1000, new Color(154, 205, 50), true);
-           createBarrierRange(-1, -1, -1, 22, new Color(153, 0, 76, 255), true);
-            createBarrierRange(-1, -1, 39, -1, new Color(0, 0, 128, 255), true);
-            createBarrierRange(-1, 22, 39, 22, new Color(0, 0, 128, 255), true);
-        createBarrierRange(39, 0, 1000, 1000, new Color(153, 76, 0, 255), true);
+//        barriers = new ArrayList<>();
+//             createBarrierRange(0, 13, 38, 1000, new Color(154, 205, 50), true);
+//           createBarrierRange(-1, -1, -1, 22, new Color(153, 0, 76, 255), true);
+//            createBarrierRange(-1, -1, 39, -1, new Color(0, 0, 128, 255), true);
+//            createBarrierRange(-1, 22, 39, 22, new Color(0, 0, 128, 255), true);
+//        createBarrierRange(39, 0, 1000, 1000, new Color(153, 76, 0, 255), true);
     }
 
     public void createBarrierRange(int startX, int startY, int endX, int endY, Color color, boolean breakable) {
