e0838d0639fdc64ddaed5e46d097844568235ccb
EricBeaudet221 <eric.beaudet@brentwood.ca>
2016-04-22 09:04:15 -0700

    character class beginning

--- src/superhot/Blue.java
+++ src/superhot/Blue.java
@@ -5,10 +5,32 @@
  */
 package superhot;
 
+import environment.Direction;
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  *
  * @author ericbeaudet
  */
 public class Blue {
     
+    public Blue(){
+        this.direction = direction;
+        
+        body = new ArrayList<>();
+        body.add(new Point(10, 0));
+    }
+    private Direction direction;
+    private ArrayList<Point> body = new ArrayList<>();
+    private Color bodyColor = Color.GREEN;
+    
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    
+    public Direction getDirection() {
+        return direction;
+    }
 }
